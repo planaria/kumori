5918a4ad4009479279c933ee1b004d75fc2c1c50
Suzuki Kojiro <pn.suzuki.kojiro@gmail.com>
2016-03-01 20:19:28 +0900

    Make static_content modifiable

--- include/kumori/static_content.hpp
+++ include/kumori/static_content.hpp
@@ -12,34 +12,27 @@ namespace kumori
 	{
 	public:
 
-		static_content(std::string content, std::string mime_type)
-			: mime_type_(std::move(mime_type))
+		static_content()
 		{
-			std::string content_gzip = compress_gzip(content);
-			std::string content_deflate = compress_deflate(content);
+		}
 
-			e_tag_ = encode_base64(hash_md5(content));
-			last_modified_ = format_rfc1123(boost::posix_time::second_clock::universal_time());
-
-			if (content_gzip.size() < content.size())
-				encoded_contents_.push_back(std::make_pair(http_content_encoding::gzip, std::move(content_gzip)));
-
-			if (content_deflate.size() < content.size())
-				encoded_contents_.push_back(std::make_pair(http_content_encoding::deflate, std::move(content_deflate)));
-
-			std::sort(encoded_contents_.begin(), encoded_contents_.end(),
-				[](const encoded_content& lhs, const encoded_content& rhs)
-			{
-				return lhs.second.size() < rhs.second.size();
-			});
-
-			encoded_contents_.push_back(std::make_pair(http_content_encoding::identity, std::move(content)));
+		static_content(std::string content, std::string mime_type)
+		{
+			set_mime_type(std::move(mime_type));
+			set_content(std::move(content));
 		}
 
 		virtual void on_get(http_server_context& context, bool head) override
 		{
 			http_request& req = context.request();
 			http_response& res = context.response();
+
+			if (encoded_contents_.empty())
+			{
+				context.response().set_content_length(0);
+				context.write_headers();
+				return;
+			}
 
 			if (req.get_cached_e_tag() == e_tag_ || req.get_cached_date() == last_modified_)
 			{
@@ -71,6 +64,40 @@ namespace kumori
 				return;
 
 			output(it->second, context.response_stream());
+		}
+
+		void set_mime_type(std::string mime_type)
+		{
+			mime_type_ = std::move(mime_type);
+		}
+
+		void set_content(std::string content)
+		{
+			std::string content_gzip = compress_gzip(content);
+			std::string content_deflate = compress_deflate(content);
+
+			std::string e_tag = encode_base64(hash_md5(content));
+			std::string last_modified = format_rfc1123(boost::posix_time::second_clock::universal_time());
+
+			std::vector<encoded_content> encoded_contents;
+
+			if (content_gzip.size() < content.size())
+				encoded_contents.push_back(std::make_pair(http_content_encoding::gzip, std::move(content_gzip)));
+
+			if (content_deflate.size() < content.size())
+				encoded_contents.push_back(std::make_pair(http_content_encoding::deflate, std::move(content_deflate)));
+
+			std::sort(encoded_contents.begin(), encoded_contents.end(),
+				[](const encoded_content& lhs, const encoded_content& rhs)
+			{
+				return lhs.second.size() < rhs.second.size();
+			});
+
+			encoded_contents.push_back(std::make_pair(http_content_encoding::identity, std::move(content)));
+
+			encoded_contents_.swap(encoded_contents);
+			e_tag_.swap(e_tag);
+			last_modified_.swap(last_modified);
 		}
 
 	private:
