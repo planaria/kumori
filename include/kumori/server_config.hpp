e4bb88759010f8fd7d7e7d2767b7bc413a86e5d6
Suzuki Kojiro <pn.suzuki.kojiro@gmail.com>
2016-03-07 18:59:00 +0900

    Undo changing port-number

--- include/kumori/server_config.hpp
+++ include/kumori/server_config.hpp
@@ -7,7 +7,7 @@ namespace kumori
 	{
 
 		bool enable_plain = true;
-		unsigned short plain_port = 8080;
+		unsigned short plain_port = 80;
 
 		bool enable_ssl = false;
 		unsigned short ssl_port = 443;
